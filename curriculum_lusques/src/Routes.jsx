5d54e9db0e1594c07643291281c7c9fd6f2b5b30
Lusques <lucascarlosjobs@gmail.com>
2022-12-20 23:02:14 -0300

    page - sobre mim

--- curriculum_lusques/src/Routes.jsx
+++ curriculum_lusques/src/Routes.jsx
@@ -1,12 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
 
 export default function ReactRoutes() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <header>
+        <Header />
+      </header>
+      <main className="container px-5">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
     </Router>
   );
 }
--- curriculum_lusques/src/pages/Home/index.jsx
+++ curriculum_lusques/src/pages/Home/index.jsx
@@ -1,10 +1,62 @@
 import Button from "../../components/Button";
 import Header from "../../components/Header";
+import style from "./Home.module.scss";
+import cake from "./images/cake.svg";
+import location from "./images/location.svg";
+import profile from "./images/profile.png";
 
 export default function Home() {
+  const currentYear = new Date().getFullYear();
+  const information = {
+    ["- O quê procuro:"]:
+      " Ambientes que agucem minha criatividade e meu conhecimento, incluindo desafios que demandem o uso de novas ferramentas/softwares.",
+    ["- Tenho conhecimento em:"]: " HTML, CSS3, Javascript, Git.",
+    ["- Pré-processadores e frameworks Css:"]: " SASS, Bootstrap e ReactJs.",
+    ["- Também domino:"]: "Figma, Trello, Jira e autogerenciamento.",
+  };
   return (
     <>
-      <Header />
+      <div className="page d-flex flex-column flex-lg-row-reverse align-items-center justify-content-between">
+        <aside
+          className={`${style.profile_image_container} d-inline-block mb-5`}
+        >
+          <span className={`${style.background_square}`}></span>
+          <div className={`${style.image_container} mb-0`}>
+            <figure>
+              <img className="w-100" src={profile} alt="foto de perfil" />
+            </figure>
+          </div>
+        </aside>
+        <div
+          className={`${style.information} headline-sm body-res-lg text-white w-75 pb-5`}
+        >
+          <h2 className="page-title display-md headline-res-md">Sobre mim</h2>
+          <ul className="list-unstyled d-flex gap-3 mt-4 mb-5 justify-content-center justify-content-lg-start">
+            <li className="d-flex align-items-center gap-2">
+              <figure className="m-0">
+                <img src={cake} alt="bolo de aniversário" />
+              </figure>
+              <p className="m-0">{currentYear - 1999} anos</p>
+            </li>
+            <li className="d-flex align-items-center gap-2">
+              <figure className="m-0">
+                <img src={location} alt="bolo de aniversário" />
+              </figure>
+              <p className="m-0">São Paulo</p>
+            </li>
+          </ul>
+          <p>
+            Chamo-me Lucas, {currentYear - 1999} anos, solteiro e residente em
+            São Paulo.
+          </p>
+          {Object.keys(information).map((value) => (
+            <p>
+              <span className="text-yellow-400">{value}</span>
+              {information[value]}
+            </p>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
